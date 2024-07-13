f39585b48f0fa6318177b6e066d51eac75cf88b1
Bhargav <75622003+BhargavEkbote@users.noreply.github.com>
2024-07-13 16:34:04 +0530

    Complete.
    The whole thing is now complete.

--- main.js
+++ main.js
@@ -5,6 +5,7 @@ leftWristY = 0;
 rightWristX = 0;
 rightWristY = 0;
 scoreLeftWrist = 0;
+scoreRightWrist = 0;
 
 function preload(){
     song = loadSound("music.mp3");
@@ -30,6 +31,28 @@ function draw(){
     image(video, 0, 0, 700, 600);
     fill("#FF0000");
     stroke("#FFOOOO");
+    if (scoreRightWrist>0.2) {
+        
+    
+    circle(rightWristX, rightWristY, 20);
+    if (rightWristY>0 && rightWristY<=100) {
+        document.getElementById("speed").innerHTML = "speed = 0.5x";
+        song.rate(0.5);
+
+    } else if(rightWristY>100 && rightWristY<=200) {
+        document.getElementById("speed").innerHTML = "speed = 1x";
+        song.rate(1);
+    } else if(rightWristY>200 && rightWristY<=300) {
+        document.getElementById("speed").innerHTML = "speed = 1.5x";
+        song.rate(1.5);
+    } else if(rightWristY>300 && rightWristY<=400) {
+        document.getElementById("speed").innerHTML = "speed = 2x";
+        song.rate(2);
+    } else if(rightWristY>400 && rightWristY<=500) {
+        document.getElementById("speed").innerHTML = "speed = 2.5x";
+        song.rate(2.5);
+    } 
+}
     if(scoreLeftWrist>0.2){
 
     
@@ -52,8 +75,9 @@ function gotPoses(results){
 
     if(results.length>0){
         console.log(results);
+        scoreRightWrist = results[0].pose.keypoints[10].score;
         scoreLeftWrist = results[0].pose.keypoints[9].score;
-        console.log("Score left wrist = "+scoreLeftWrist);
+        console.log("Score left wrist = "+scoreLeftWrist + " Score right wrist = "+scoreRightWrist);
         //Left Wrist
         leftWristX = results[0].pose.leftWrist.x;
         leftWristY = results[0].pose.leftWrist.y;
